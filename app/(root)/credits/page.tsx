b57914d48bb89a5fac342a09c5c235cea6e941dd
Adyant <adyantajay@gmail.com>
2024-06-09 09:25:57 +0530

    Initial Commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreditsPage = () => {
+  return (
+    <div>
+      Credit
+    </div>
+  )
+}
+
+export default CreditsPage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>
+      Add
+    </div>
+  )
+}
+
+export default AddTransformationTypePage
